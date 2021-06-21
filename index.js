6be86bd7256a14895e5dc7643c882b487f2b8d1c
unknown <jchernandez827@gmail.com>
2021-06-21 12:15:59 -0500

    Remove Selection from css

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+let menu = document.querySelector('#menu-mobile');
+
 function openMenu() {
-    let menu = document.querySelector('#menu-mobile');
     menu.style.visibility = 'visible';
 }
 
-
 function closeMenu() {
-    let menu = document.querySelector('#menu-mobile');
     menu.style.visibility = 'hidden';
 }
 
