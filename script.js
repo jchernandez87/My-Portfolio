17aac7585939d86207640561ffd61c6c12b99bce
unknown <jchernandez827@gmail.com>
2021-06-21 17:33:56 -0500

    Javscript events change to EvenListeners

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const menu = document.querySelector('#menu-dropdown');
+const btn = document.querySelector('.btn');
+const btnClose = document.querySelector('.close-btn');
+const link = document.querySelector('.btn-link');
+const link2 = document.querySelector('.btn-link2');
+const link3 = document.querySelector('.btn-link3');
 
-const opened = function openMenu() {
-    menu.style.visibility = 'visible';
-}
+function openMenu() {
+  menu.style.visibility = 'visible';
+  console.log(links)
+};
 
-const closed = function closeMenu() {
-    menu.style.visibility = 'hidden';
-}
+function closeMenu() {
+  menu.style.visibility = 'hidden';
+};
+
+btn.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+link.addEventListener('click', closeMenu);
+link2.addEventListener('click', closeMenu);
+link3.addEventListener('click', closeMenu);
 
