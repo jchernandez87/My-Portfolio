b5b05397032b0e54f2e67a349ec8ece7adbcba54
unknown <jchernandez827@gmail.com>
2021-06-21 11:54:36 -0500

    Add mobile menu

---
+++ index.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    let menu = document.querySelector('#menu-mobile');
+    menu.style.visibility = 'visible';
+}
+
+
+function closeMenu() {
+    let menu = document.querySelector('#menu-mobile');
+    menu.style.visibility = 'hidden';
+}
+
