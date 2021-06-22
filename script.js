9eceaf3e11c214cfc5231f04b9e9c846989040af
unknown <jchernandez827@gmail.com>
2021-06-22 14:46:24 -0500

    Add modal for mobile

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 /* eslint-disable linebreak-style */
 const menu = document.querySelector('#menu-dropdown');
+const modal = document.querySelector('#modal-small');
+const overlay = document.querySelector('#overlay');
 const btn = document.querySelector('.btn');
 const btnClose = document.querySelector('.close-btn');
 const link = document.querySelector('.btn-link');
 const link2 = document.querySelector('.btn-link2');
 const link3 = document.querySelector('.btn-link3');
+const modalOpenBtn = document.querySelector('.projectBtn');
+const modalCloseBtn = document.querySelector('#close-modal-btn');
 
 function openMenu() {
   menu.style.visibility = 'visible';
@@ -19,3 +23,16 @@ btnClose.addEventListener('click', closeMenu);
 link.addEventListener('click', closeMenu);
 link2.addEventListener('click', closeMenu);
 link3.addEventListener('click', closeMenu);
+
+function openModal() {
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal() {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+modalOpenBtn.addEventListener('click', openModal);
+modalCloseBtn.addEventListener('click', closeModal);
